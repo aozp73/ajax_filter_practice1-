086176d6995c687f6402581771a463efb037c182
aozp73 <aozp73@naver.com>
2023-02-02 21:09:27 +0900

    세팅 완료

---
+++ src/main/java/shop/mtcoding/filter1/Filter1Application.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.filter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Filter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Filter1Application.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/filter1/Filter1ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.filter1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Filter1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
